6f7c29a95e88e8ee445179065e3e3f3bddecbd03
Ahmed Hisham <a7med.hish@gmail.com>
2020-09-04 02:15:36 +0200

    orientations of corners

--- utils.py
+++ utils.py
@@ -2,16 +2,15 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def FAST(img, N=9, threshold=0.15):
-    cross_idx = np.array([[3,0,-3,0], [0,3,0,-3]])
-    circle_idx = np.array([[3,3,2,1,0,-1,-2,-3,-3,-3,-2,-1,0,1,2,3],
-	                       [0,1,2,3,3,3,2,1,0,-1,-2,-3,-3,-3,-2,-1]])
-
     kernel = np.array([[1,2,1],
                        [2,4,2],
                        [1,2,1]])/16      # 3x3 Gaussian Window
     
     img = convolve2d(img, kernel, mode='same')
 
+    cross_idx = np.array([[3,0,-3,0], [0,3,0,-3]])
+    circle_idx = np.array([[3,3,2,1,0,-1,-2,-3,-3,-3,-2,-1,0,1,2,3],
+	                       [0,1,2,3,3,3,2,1,0,-1,-2,-3,-3,-3,-2,-1]])
     keypoints = []
     for y in range(3, img.shape[0]-3):
         for x in range(3, img.shape[1]-3):
@@ -24,7 +23,32 @@ def FAST(img, N=9, threshold=0.15):
                     # Keypoint [corner]
                     keypoints.append([x,y])
 
-    return keypoints
+    return np.array(keypoints)
+
+def corner_orientations(img, corners, mask):
+    mrows, mcols = mask.shape
+    
+    mrows2 = int((mrows - 1) / 2)
+    mcols2 = int((mcols - 1) / 2)
+    
+    img = np.pad(img, (mrows2, mcols2), mode='constant', constant_values=0)
+
+    orientations = []
+    for corner in corners:
+        c0, r0 = corner
+        m01, m10 = 0, 0
+        for r in range(mrows):
+            m01_temp = 0
+            for c in range(mcols):
+                if mask[r,c]:
+                    I = img[r0+r, c0+c]
+                    m10 = m10 + I*(c-mcols2)
+                    m01_temp = m01_temp + I
+            m01 = m01 + m01_temp*(r-mrows2)
+        orientations.append(np.arctan2(m01, m10))
+
+    return np.array(orientations)
+
 
 
 if __name__ == "__main__":
@@ -33,6 +57,7 @@ if __name__ == "__main__":
     from time import time
 
     img = cv2.imread('images/waffle.jpg')
+    original_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
@@ -66,3 +91,14 @@ if __name__ == "__main__":
 
     print('my keypoints: ', len(keypoints), '\ncv keypoints: ', len(kp))
     
+    print('corner orientations')
+    from skimage.morphology import octagon
+    orientations = corner_orientations(gray, keypoints, octagon(3,2))
+    print(np.rad2deg(orientations))
+
+    plt.figure()
+    plt.imshow(original_img)
+    for i in range(keypoints.shape[0]):
+        plt.quiver(keypoints[i, 0], keypoints[i, 1], np.cos(orientations[i]), np.sin(orientations[i]), 
+                width=0.002, headwidth=5, scale=30)
+    plt.show()
